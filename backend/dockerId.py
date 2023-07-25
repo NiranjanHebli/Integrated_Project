b259caaec65fce1acc171135b640377356dd2c8c
root <root@ip-172-31-43-148.eu-north-1.compute.internal>
2023-07-25 14:43:36 +0000

    -a

---
+++ backend/dockerId.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+print("Content-type: text/html")
+print()
+
+import subprocess
+import cgi
+
+cmd = "sudo docker ps -aq"
+output = subprocess.getoutput(cmd)
+
+print("""
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100vh;
+        font-family: Arial, sans-serif;
+        background-color: #000000;
+        color: #ffffff;
+    }
+    .output {
+        color: cyan;
+        background-color: grey;
+        width: 50%;
+        height: 50vh;
+    }
+    .header {
+        font-size: 30px;
+        color: #4CAF50;
+        margin-bottom: 20px;
+    }
+    .footer {
+        font-size: 12px;
+        color: #4CAF50;
+        margin-top: 20px;
+    }
+</style>
+</head>
+<body>
+""")
+
+# Print Header
+print("<div class='header'>Docker All Instances IDs</div>")
+
+# Output Textbox
+print("<textarea class='output' readonly>{}</textarea>".format(output))
+
+# Print Footer
+print("<div class='footer'>Copywrite @ Team Turtle</div>")
+
+# End of body and html
+print("""
+</body>
+</html>
+""")
